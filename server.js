7ef301e6cc727b19d532358d88da89e24bc1b097
WillePille69xx <william.grenholm@elev.ga.ntig.se>
2025-09-02 08:48:25 +0200

    Add nodemon as a dev dependency and update server response format

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ import express from "express"
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    console.log(res)
+    res.send("<h1>Hello World!<h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.json({
+        "message": "Hellohaso"
+    })
 })
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on http://localhost:3000")
 })
